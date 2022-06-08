e31600cf4b04513d7eed7f043b5c44c15efb36ae
Rasif The Legend <rasif.outdesk@gmail.com>
2022-06-08 23:55:07 +0530

    top navigation is completed

--- src/js/Navigation/Navigation.jsx
+++ src/js/Navigation/Navigation.jsx
@@ -1,44 +1,57 @@
-// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-// import CodingStyle from '../Pages/Main/Content/CodingStyle';
-// import Latest from '../Pages/NewFeatures/Latest';
-
-// export default function Navigation() 
-// {
-//     return <div className="navigation">
-        
-//         <Router className="app-header">
-//             <div>
-//                 <nav>
-//                     <ul>
-//                         <li><Link to={'/'} className="nav-link"> Home </Link></li>
-//                         <li><Link to={'/updates'} className="nav-link">Latest Updates</Link></li>
-//                     </ul>
-//                 </nav>
-//                 <hr />
-//                 <Routes>
-//                     <Route exact path='/' element={<CodingStyle />} />
-//                     <Route path='/updates' element={<Latest/>} />
-//                 </Routes>
-//             </div>
-//         </Router>
-
-//     </div>
-// }
-
-import React from 'react';
+import React, { useState } from 'react';
 import {  Link } from "react-router-dom";
 import '../../scss/navigation.scss';
 
 export default function Navigation() 
 {
-  return (
-  <div className="navigation">
-    <li>
-      <Link to="/">Home</Link>
-    </li>
-    <li>
-      <Link to="/updates">Updates</Link>
-    </li>
-  </div>
-  );
+    const [isNavExpanded, setIsNavExpanded] = useState(false)
+    return (
+//   <div className="navigation">
+//     <li>
+//       <Link to="/">Home</Link>
+//     </li>
+//     <li>
+//       <Link to="/updates">Updates</Link>
+//     </li>
+//   </div>
+
+        <nav className="navigation">
+            <a href="/" className="brand-name">
+                PHP Tutorial
+            </a>
+            <button className="hamburger" 
+                onClick={() => {
+                    setIsNavExpanded(!isNavExpanded);
+                }}
+            >
+                {/* icon from heroicons.com */}
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="white"
+                >
+                    <path
+                        fillRule="evenodd"
+                        d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+                        clipRule="evenodd"
+                    />
+                </svg>
+            </button>
+            <div className={
+                    isNavExpanded ? "expanded" : "navigation-menu"
+                }
+            >
+                <ul>
+                    <li>
+                        <a href="/">Introduction</a>
+                    </li>
+                    <li>
+                        <a href="/updates">Updates</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+    );
 }
